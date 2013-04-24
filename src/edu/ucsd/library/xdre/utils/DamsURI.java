5a6dc2951f0c016cf1f7397afd03e2ed2e3c86cc
lsitu <lsitu@PC11757.AD.UCSD.EDU>
2013-04-24 12:41:56 -0700

    Extended to parse a DAMS URL with a type like File, Component, Object.

--- src/edu/ucsd/library/xdre/utils/DamsURI.java
+++ src/edu/ucsd/library/xdre/utils/DamsURI.java
@@ -6,6 +6,9 @@ package edu.ucsd.library.xdre.utils;
  *
  */
 public class DamsURI {
+	public static final String FILE = "File";
+	public static final String COMPONENT = "Component";
+	public static final String OBJECT = "Object";
 	private String object = null;
 	private String component = null;
 	private String fileName = null;
@@ -54,7 +57,7 @@ public class DamsURI {
 	}
 	
 	public boolean isFileURI(){
-		return object != null && object.length()>0 && fileName!=null && fileName.indexOf(".")>0;
+		return object != null && object.length()>0 && fileName!=null;
 	}
 	
 	public String toString(){
@@ -63,68 +66,97 @@ public class DamsURI {
 	
 	/**
 	 * Parse a file URL for the file ID parts
-	 * @param fileURI
-	 * @param object
+	 * @param damsUri
+	 * @param object: object id
 	 * @return
 	 * @throws Exception 
 	 */
-	public static DamsURI toParts(String fileURI, String object) throws Exception{
+	public static DamsURI toParts(String damsUri, String object) throws Exception{
+		return toParts(damsUri, object, null);
+	}
+	
+	/**
+	 * Parse a file URL for the file ID parts with record type
+	 * @param damsUri
+	 * @param object: object id
+	 * @param type: File, Component, Object
+	 * @return
+	 * @throws Exception 
+	 */
+	public static DamsURI toParts(String damsUri, String object, String type) throws Exception{
 		String component = null;
 		String fileName = null;
 		String idString = null;
 		int idx = -1;
-		if(object != null && object.length() > 0 && fileURI.indexOf(object) == 0 && fileURI.length() > object.length()){
-			idString = fileURI.substring(object.length()+1);
-			idx = idString.indexOf("/");
-			if(idx > 0){
-				component = idString.substring(0, idx);
-				fileName = idString.substring(idx + 1);
-			} else {
-				try{
-					Integer.parseInt(idString);
-					component = idString;
-				}catch (NumberFormatException ne){
-					fileName = idString;
-				}
-			}
-		} else if ((idx=fileURI.indexOf("ark:/")) >= 0){
-			idString = fileURI.substring(idx+6);
-			String[] tmp = idString.split("/");
+		String[] tmp = null;
+		if(object != null && object.length() > 0 && damsUri.indexOf(object) == 0 && damsUri.length() > object.length()){
+			// object/cid/fid
+			idString = damsUri.substring(object.length()+1);
+			tmp = idString.split("/");
+			if (tmp.length == 2) {
+				component = tmp[0];
+				fileName = tmp[1];
+			} else if(tmp.length == 1) {			
+				if(isFileId(tmp[0], type))
+					fileName = tmp[0];
+				else
+					component = tmp[0];	
+			} else
+				throw new Exception("Unknown object/file URL format: " + damsUri);
+		} else if ((idx=damsUri.indexOf("ark:/")) >= 0){
+			// /ark:/20775/cid/fid
+			idString = damsUri.substring(idx+6);
+			tmp = idString.split("/");
 			int len = tmp.length;
-			object = fileURI.substring(0, fileURI.indexOf(tmp[1]))+tmp[1];
+			object = damsUri.substring(0, damsUri.indexOf(tmp[1]))+tmp[1];
 			if(len == 4){
 				component = tmp[2];
 				fileName = tmp[3];
 			}else if(len == 3){
-				try{
-					Integer.parseInt(tmp[2]);
+				if(isFileId(tmp[2], type) && !tmp[2].toLowerCase().startsWith("cid"))
+					fileName = tmp[2];
+				else
 					component = tmp[2];
-				}catch (NumberFormatException ne){
-					if(tmp[2].toLowerCase().startsWith("cid"))
-						component = tmp[2];
-					else
-						fileName = tmp[2];
-				}
 			}else if(len != 2)
-				throw new Exception("Unknown object/file URL format: " + fileURI);
-		}else {
-			String[] tmp = fileURI.split("/");
+				throw new Exception("Unknown object/file URL format: " + damsUri);
+		} else {
+			// oid/cid/fid
+			tmp = damsUri.split("/");
 			int len = tmp.length;
 			if(tmp.length > 3)
-				throw new Exception("Unhandled object/file URL format: " + fileURI);
-			object = fileURI.substring(0, fileURI.indexOf(tmp[0]))+tmp[0];
+				throw new Exception("Unhandled object/file URL format: " + damsUri);
+			object = damsUri.substring(0, damsUri.indexOf(tmp[0]))+tmp[0];
 			if(len == 3){
 				component = tmp[1];
 				fileName = tmp[2];
-			}else{
-				try{
-					Integer.parseInt(tmp[1]);
-					component = tmp[1];
-				}catch (NumberFormatException ne){
+			} else if(len == 2){
+				if(isFileId(tmp[1], type) && !tmp[1].toLowerCase().startsWith("cid"))
 					fileName = tmp[1];
-				}
+				else
+					component = tmp[1];
 			}
 		}
 		return new DamsURI(object, component, fileName);
+	}
+	
+	/**
+	 * Determine a file id
+	 * @param value
+	 * @param type
+	 * @return
+	 */
+	private static boolean isFileId(String value, String type){
+		boolean isFid = false;
+		if(type != null && type.endsWith(FILE)){
+			isFid = true;
+		} else {
+			// Guess it as a component when there is no file extension
+			try{
+				Integer.parseInt(value);
+			}catch (NumberFormatException ne){
+				isFid = true;
+			}
+		}
+		return isFid;
 	}
 }
