4b4b7ef9244a95b0bbe30bd4b02b5cae40092af2
Longshou Situ <lsitu@ucsd.edu>
2014-02-04 17:37:45 -0800

    Fixed the URL to display the icon when logging in.

--- src/edu/ucsd/library/xdre/web/StatsPopularController.java
+++ src/edu/ucsd/library/xdre/web/StatsPopularController.java
@@ -101,7 +101,7 @@ public class StatsPopularController implements Controller {
 		String data = "";
 		String solrQuery = Constants.SOLR_URL_BASE + "/select?q=id:(" + subjects+ ")&fl=id+title_tesim+collections_tesim+collection_name_tesim+discover_access_group_ssim+*files_tesim+component_count_isi&rows=200&wt=json";
 		DAMSClient damsClient = null;
-		String clusterHost = "//" + Constants.CLUSTER_HOST_NAME + ".ucsd.edu";
+		String clusterHost = "http://" + Constants.CLUSTER_HOST_NAME + ".ucsd.edu";
 		try {
 			damsClient = DAMSClient.getInstance();
 			data = damsClient.getContentBodyAsString(solrQuery);
