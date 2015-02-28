77c78caf1e6d8dfde2f2e6b048d485579871c6f0
lsitu <lsitu@PC11757.AD.UCSD.EDU>
2015-02-27 17:10:44 -0800

    Added validation and error report for column names and control values, updated to use the first sheet. (DM-87)

--- src/edu/ucsd/library/xdre/tab/ExcelSource.java
+++ src/edu/ucsd/library/xdre/tab/ExcelSource.java
@@ -14,9 +14,12 @@ import java.io.UnsupportedEncodingException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -27,16 +30,22 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 /**
  * RecordSource implementation that uses Apache POI to read Excel (OLE or XML)
  * files.
+ * @author lsitu
  * @author escowles
  * @since 2014-06-05
 **/
 public class ExcelSource implements RecordSource
 {
+    private static Map<String, List<String>> CONTROL_VALUES = new HashMap<>();
+    
     private Workbook book;
     private Sheet sheet;
     private int lastRow;
     private int currRow;
     private List<String> headers;
+    private Map<String, String> originalHeaders = new HashMap<>();
+    private List<String> invalidHeaders = new ArrayList<>();
+    private List<Map<String, String>> invalidValues = new ArrayList<>();
     Map<String,String> cache;
 
     /**
@@ -55,16 +64,8 @@ public class ExcelSource implements RecordSource
     {
         this.book = WorkbookFactory.create(in);
         
-        // use the the first sheet when there's only one sheet.
-        int numberOfSheets = book.getNumberOfSheets();
-        if (numberOfSheets == 1)
-        {
-        	this.sheet = book.getSheetAt(0);
-        }
-        else
-        {
-            this.sheet = book.getSheetAt(1);
-        }
+        // always use the the first sheet.
+        this.sheet = book.getSheetAt(0);
         
         this.lastRow = sheet.getLastRowNum();
         if ( this.lastRow == 0 )
@@ -79,7 +80,17 @@ public class ExcelSource implements RecordSource
             Row firstRow = sheet.getRow(0);
             for ( int i = 0; i < firstRow.getLastCellNum(); i++ )
             {
-                headers.add( firstRow.getCell(i).getStringCellValue().toLowerCase() );
+            	String header = firstRow.getCell(i).getStringCellValue();
+            	String lcHeader = header.toLowerCase();
+                headers.add( lcHeader );
+                
+                // keep the original header for error report
+                originalHeaders.put(lcHeader, header);
+                if ( CONTROL_VALUES != null && CONTROL_VALUES.size() > 0 
+                		&& StringUtils.isNotBlank(lcHeader) && !CONTROL_VALUES.containsKey(lcHeader) )
+                {
+                	invalidHeaders.add(header);     		
+                }
             }
             currRow++;
             cache = parseRow( currRow );
@@ -156,37 +167,162 @@ public class ExcelSource implements RecordSource
     {
         Row row = sheet.getRow(n);
         Map<String,String> values = new HashMap<>();
-        for ( int i = 0; i < headers.size(); i++ )
+        Map<String,String> invalids = new TreeMap<>();
+        if (row != null) 
         {
-            String header = headers.get(i);
-            String value = null;
-            if ( i < (row.getLastCellNum() + 1) ) 
-            {
-                Cell cell = row.getCell(i);
-                if ( cell != null )
-                {
-                	cell.setCellType(Cell.CELL_TYPE_STRING);
-                    value = cell.toString();
-                }
-            }
-            if ( value != null && !value.trim().equals("") )
-            {
-                try {
-                    value = new String(value.trim().getBytes("UTF-8"));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                String existing = values.get(header);
-                if ( existing == null )
-                {
-                    values.put(header, value);
-                }
-                else
-                {
-                    values.put(header, existing + DELIMITER + value);
-                }
-            }
+	        for ( int i = 0; i < headers.size(); i++ )
+	        {
+	            String header = headers.get(i);
+	            String value = null;
+	            if ( i < (row.getLastCellNum() + 1) ) 
+	            {
+	                Cell cell = row.getCell(i);
+	                if ( cell != null )
+	                {
+	                	cell.setCellType(Cell.CELL_TYPE_STRING);
+	                    value = cell.toString();
+	                }
+	            }
+	            if ( value != null && !value.trim().equals("") )
+	            {
+	                try {
+	                    value = new String(value.trim().getBytes("UTF-8"));
+
+	                    // check for invalid control values
+	                    List<String> validValue = CONTROL_VALUES.get(header);
+	                    if (validValue != null && validValue.size() > 0 && !validValue.contains(value.toLowerCase())) 
+	                    {
+	    	                String existing = invalids.get(header);
+	    	                if ( existing == null )
+	    	                	invalids.put(originalHeaders.get(header), value);
+	    	                else
+	    	                    values.put(originalHeaders.get(header), existing + DELIMITER + value);
+	                    }
+	                } catch (UnsupportedEncodingException e) {
+	                    e.printStackTrace();
+	                }
+	                String existing = values.get(header);
+	                if ( existing == null )
+	                {
+	                    values.put(header, value);
+	                }
+	                else
+	                {
+	                    values.put(header, existing + DELIMITER + value);
+	                }
+	            }
+	        }
+	        
+	        if (invalids.size() > 0) 
+	        {
+	        	invalids.put("row", "" + (n + 1));
+	        	invalids.put(TabularRecord.OBJECT_ID, values.get(TabularRecord.OBJECT_ID));
+	        	invalidValues.add(invalids);
+	        }
         }
         return values;
+    }
+
+    /**
+     * Get the list of invalid columns
+     * @return
+     */
+    public List<String> getInvalidColumns()
+    {
+    	return invalidHeaders;
+    }
+
+    /**
+     * Get the list of invalid values
+     * @return
+     */
+    public List<Map<String, String>> getInvalidValues()
+    {
+    	return invalidValues;
+    }
+
+    /**
+     * Get the Control Values
+     * @return
+     */
+    public Map<String, List<String>> getControlValues()
+    {
+    	return CONTROL_VALUES;
+    }
+
+    /**
+     * Initiate control values for column names and field values
+     * @param columns
+     * @param cvs
+     * @throws IOException 
+     * @throws InvalidFormatException 
+     */
+    public synchronized static void initControlValues(File columns, File cvs) throws InvalidFormatException, IOException 
+    {
+    	if ( CONTROL_VALUES == null || CONTROL_VALUES.size() == 0 )
+    	{
+    		CONTROL_VALUES = new HashMap<>();
+    		List<String> cvHeaders = new ArrayList<>();
+    		
+    		// Initiate control values
+    		Workbook book = WorkbookFactory.create(cvs);
+    		Sheet sheet = book.getSheetAt(0);
+    		for ( Iterator<Row> it = sheet.rowIterator(); it.hasNext(); )
+    		{
+    			Row row = it.next();
+				int lastCellNum = row.getLastCellNum();
+				for ( int i = 0; i < lastCellNum; i++ )
+				{
+					Cell cell = row.getCell(i);
+					if ( cell != null )
+					{
+						cell.setCellType(Cell.CELL_TYPE_STRING);
+	                    String value = cell.toString();
+	                    if ( value != null && !(value = value.trim()).equals("") )
+	    	            {
+	                    	value = value.toLowerCase();
+							if (row.getRowNum() == 0) 
+							{
+								cvHeaders.add( value );
+								CONTROL_VALUES.put(value, new ArrayList<String>());
+							} 
+							else
+							{
+								value = new String(value.getBytes("UTF-8"));
+								String header = cvHeaders.get(cell.getColumnIndex());
+								CONTROL_VALUES.get(header).add(value);
+							}
+	    	            }
+					}
+				}
+    		}
+    		
+    		// ARK column
+    		if (!CONTROL_VALUES.containsKey("ark"))
+    			CONTROL_VALUES.put("ark", new ArrayList<String>());
+    		
+    		// add all valid column names
+    		book = WorkbookFactory.create(columns);
+    		sheet = book.getSheetAt(0);
+    		for ( Iterator<Row> it = sheet.rowIterator(); it.hasNext(); )
+    		{
+    			Row row = it.next();
+				int lastCellNum = row.getLastCellNum();
+				for ( int i = 0; i < lastCellNum; i++ )
+				{
+					Cell cell = row.getCell(i);
+					if ( cell != null )
+					{
+						cell.setCellType(Cell.CELL_TYPE_STRING);
+	                    String value = cell.toString();
+	                    if ( value != null && !(value = value.trim()).equals("") && !CONTROL_VALUES.containsKey(value.toLowerCase()) )
+	    	            {
+	                    	value = value.toLowerCase();
+	                    	CONTROL_VALUES.put(value, new ArrayList<String>());
+	    	            }
+					}
+				}
+    		}
+    	}
     }
 }
