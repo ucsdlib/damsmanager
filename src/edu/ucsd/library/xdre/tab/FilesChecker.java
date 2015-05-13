7f88253129a784fbc2631b3fad49f1e41f1571d3
lsitu <lsitu@PC11757.AD.UCSD.EDU>
2015-05-13 12:28:52 -0700

    Added file validation feature with the ExifTool to validate the files. DM-101

--- src/edu/ucsd/library/xdre/tab/FilesChecker.java
+++ src/edu/ucsd/library/xdre/tab/FilesChecker.java
@@ -1,7 +1,12 @@
 package edu.ucsd.library.xdre.tab;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -9,6 +14,7 @@ import java.util.TreeMap;
 import org.apache.commons.lang3.StringUtils;
 
 import edu.ucsd.library.xdre.collection.CollectionHandler;
+import edu.ucsd.library.xdre.utils.DAMSClient;
 
 /**
  * FilesCheck for Files Match and Files Validation
@@ -23,6 +29,10 @@ public class FilesChecker {
 	private List<String> matchedFiles = null;
 	private List<String> missingFiles = null;
 	private Map<String, File> extraFiles = null;
+	private Map<File, String> invalidFiles = null;
+	private String command = "exiftool";
+	private static final String ERROR = "Error";
+	private static final String WARNING = "Warning";
 
 	/**
 	 * Constructor with list of source file paths
@@ -63,6 +73,7 @@ public class FilesChecker {
 		matchedFiles = new ArrayList<String> ();
 		missingFiles = new ArrayList<String> ();
 		extraFiles = new TreeMap<String, File> ();
+		invalidFiles = new TreeMap<File, String> ();
 	}
 	
 	public List<String> getMatchedFiles() {
@@ -75,6 +86,14 @@ public class FilesChecker {
 
 	public Map<String, File> getExtraFiles() {
 		return extraFiles;
+	}
+
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public Map<File, String> getInvalidFiles() {
+		return invalidFiles;
 	}
 
 	/**
@@ -95,5 +114,109 @@ public class FilesChecker {
 			}
 		}
 		return missingFiles.size() <= 0;
+	}
+
+	/**
+	 * Validate files that are matched the metadata
+	 * @return
+	 */
+	public boolean filesValidate ()
+	{
+		//validate the match files
+		if (matchedFiles.size() == 0)
+			filesMatch ();
+		for (String fileName : matchedFiles) {
+			File src = sourceFiles.get(fileName);
+			Map<String, String> m = extractMetadata(src);
+			if (m.containsKey(ERROR) || m.containsKey(WARNING)) {
+				String message = "";
+				if (m.containsKey(ERROR))
+					message = ERROR + ": " + m.get(ERROR);
+				if (m.containsKey(WARNING))
+					message += (message.length() > 0 ? " || " : "") + WARNING + ": " + m.get(WARNING);
+
+				invalidFiles.put(src, message);
+			}
+		}
+		return invalidFiles.size() <= 0;
+	}
+
+	private Map<String, String> extractMetadata(File src) 
+	{
+		Reader reader = null;
+		InputStream in = null;
+		BufferedReader buf = null;
+		StringBuffer log = null;
+		Process proc = null;
+		Map<String, String> metadata = new HashMap<String, String>();
+		try 
+		{
+			// Build the command for file validation
+			List<String> cmd = new ArrayList<>();
+			cmd.add( command );
+			cmd.add( src.getAbsolutePath());
+			
+			log = new StringBuffer();
+			// Execute the command
+			ProcessBuilder pb = new ProcessBuilder(cmd);
+			pb.redirectErrorStream(true);
+			proc = pb.start();
+			
+			in = proc.getInputStream();
+			reader = new InputStreamReader(in);
+			buf = new BufferedReader(reader);
+			for ( String line = null; (line=buf.readLine()) != null; ) 
+			{
+				String[] pair = line.split("\\:");
+				String v = "";
+				if (pair.length == 2)
+					v = StringUtils.isBlank(pair[1]) ? "" : pair[1].trim();
+
+				addMetadata(metadata, pair[0].trim(), v);
+			}
+
+			// Wait for the process to exit
+			int status = proc.waitFor();
+			if ( status != 0 ) 
+			{
+				// Output error messages
+				in = proc.getErrorStream();
+				reader = new InputStreamReader(in);
+				buf = new BufferedReader(reader);
+				for ( String line = null; (line=buf.readLine()) != null; ) 
+					log.append( line + "\n" );
+
+				addMetadata(metadata, ERROR, log.toString());
+			}
+		} 
+		catch ( Exception ex ) 
+		{
+			ex.printStackTrace();
+			addMetadata(metadata, ERROR, ex.getMessage());
+		} 
+		finally 
+		{
+			DAMSClient.close(in);
+			DAMSClient.close(reader);
+			DAMSClient.close(buf);
+			if(proc != null)
+			{
+				proc.destroy();
+				proc = null;
+			}
+		}
+		return metadata;
+	}
+
+	private void addMetadata(Map<String, String> metadata, String name, String value)
+	{
+		String v = metadata.get(name);
+		// concatenate the values with pine
+		if (StringUtils.isNotBlank(v) && StringUtils.isNotBlank(value))
+			v += " | " + value;
+		else if (StringUtils.isNotBlank(value))
+			v = value;
+
+		metadata.put(name, v);
 	}
 }
