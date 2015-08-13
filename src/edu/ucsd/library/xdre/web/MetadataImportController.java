0c0ecf7a345de415154a5fecce73f6b0ca25b69b
lsitu <lsitu@PC11757.AD.UCSD.EDU>
2015-08-12 17:14:11 -0700

    Refactored sparql query with more specific predicates to improve performance, update authoritativeLabel for ComplexSubjects. DHH-699

--- src/edu/ucsd/library/xdre/web/MetadataImportController.java
+++ src/edu/ucsd/library/xdre/web/MetadataImportController.java
@@ -76,8 +76,8 @@ public class MetadataImportController implements Controller{
 						String className = node.getParent().getName();
 
 						if (!className.endsWith("Object")) {
-							// Collection or Authority update, need to update SOLR for the linked records.
-							queryObjects ( soluctions, subId, 1, damsClient );
+							// Collection or Authority update, need to update SOLR for the linked records. Recursive lookup for collections.
+							queryObjects ( soluctions, subId, 1, damsClient, doc );
 							List<String> solrFaileds = new ArrayList<>();
 							for (String soluction : soluctions) {
 								boolean successful = false;
@@ -91,7 +91,7 @@ public class MetadataImportController implements Controller{
 									solrFaileds.add(soluction);
 								}
 							}
-							objLink = "Total " + soluctions.size() + " records affected by ark " + subId.substring(subId.lastIndexOf("/") + 1) + ".";
+							objLink = "Total " + (soluctions.size() - 1) + " records affected by ark " + subId.substring(subId.lastIndexOf("/") + 1) + ".";
 							if (solrFaileds.size() > 0) {
 								StringBuilder builder = new StringBuilder();
 								for (String id : solrFaileds) {
@@ -141,58 +141,132 @@ public class MetadataImportController implements Controller{
 		return null;
 	}
 
-	private void queryObjects( List<String> soluctions, String oid, int level, DAMSClient damsClient ) throws Exception {
-		String sparql = "SELECT ?sub WHERE {";
-		String bnId = "";
+	private void queryObjects( List<String> soluctions, String oid, int level, DAMSClient damsClient, Document parent ) throws Exception {
 		int le = level;
-		for ( int i = 0; i < le; i++ ) {
-			if ( i == 0 && le <= 1 )
-				sparql += " ?sub ?pre" + i + " <" + oid + "> ";
-			else if ( i == 0 ) {
-				bnId = "_bn" + i;
-				sparql += " ?sub ?pre" + i + " ?" + bnId;
-			} else if ( i == le - 1 ) {
-				sparql += " . ?" + bnId + " ?pre" + i + " <" + oid + "> ";
-			} else {
-				String tmpBnId = "_bn" + i;
-				sparql += " . ?" + bnId + " ?pre" + i + " ?" + tmpBnId;
-				bnId= tmpBnId;
-			}	
-		}
-		sparql += "}";
-
-		soluctions.add(oid);
-		List<Map<String, String>> results = damsClient.sparqlLookup(sparql);
-		boolean hasBn = false;
-		for ( Map<String, String> result : results ) {
-			String sub = result.get("sub");
-			if (!sub.startsWith("http")) {
-				hasBn = true;
-			} else {
-				// affected record, could be a component URI.
-				DamsURI damsURI = DamsURI.toParts(sub, null);
-				sub = damsURI.getObject();
-				if ( !soluctions.contains(sub) ) {
-					soluctions.add(sub);
-					try {
-						Document doc = damsClient.getRecord(sub);
-						if (doc.selectSingleNode("//dams:Object") == null ) {
-							// collection CLR, authority record, need to query linked records
-							queryObjects ( soluctions, sub, 1, damsClient );
+		String[] sparqls =  getQueries(oid, level, parent);
+		for (String sparql : sparqls) {
+			if (!soluctions.contains(oid))
+				soluctions.add(oid);
+			List<Map<String, String>> results = damsClient.sparqlLookup(sparql);
+			boolean hasBn = false;
+			for ( Map<String, String> result : results ) {
+				String sub = result.get("sub");
+				if (!sub.startsWith("http")) {
+					hasBn = true;
+				} else {
+					// affected record, could be a component URI.
+					DamsURI damsURI = DamsURI.toParts(sub, null);
+					sub = damsURI.getObject();
+					if ( !soluctions.contains(sub) ) {
+						soluctions.add(sub);
+						try {
+							Document doc = damsClient.getRecord(sub);
+							// recursive lookup for authority records and collections hierarchy only
+							if (doc.selectSingleNode("/rdf:RDF/*[contains(name(), 'Object') or contains(name(), 'Collection')]" ) == null
+									|| (parent.selectSingleNode("/rdf:RDF/*[contains(name(), 'Collection')]") != null 
+											&& doc.selectSingleNode("/rdf:RDF/*[contains(name(), 'Collection')]") != null)) {
+	
+								// update the authoritativeLabel for the ComplexSubject
+								boolean isComplexSubject = doc.selectSingleNode("/rdf:RDF/mads:ComplexSubject") != null;
+								if (isComplexSubject) {
+									Document fullDoc = damsClient.getFullRecord(sub);
+	
+									List<Node> subElems = fullDoc.selectNodes("/rdf:RDF/mads:ComplexSubject/mads:componentList/*/mads:authoritativeLabel");
+									if (subElems.size() > 0) {
+										String authoritativeLabel = "";
+										for ( Node subElem : subElems) {
+											authoritativeLabel += (authoritativeLabel.length() > 0 ? "--" : "") + subElem.getText();
+										}
+										
+										doc.selectSingleNode("/rdf:RDF/mads:ComplexSubject/mads:authoritativeLabel").setText(authoritativeLabel);
+										MetadataImportHandler handler = new MetadataImportHandler(damsClient, null, doc.asXML(), null, Constants.IMPORT_MODE_ALL);
+										
+										try {
+											if(handler.execute()) {
+												// need to query linked records for the authority record
+												queryObjects ( soluctions, sub, 1, damsClient, doc );
+											} else
+												throw new Exception ("Failed to update linked record " + sub);
+										} catch (Exception e) {
+											throw new Exception ("Failed to update linked record " + sub, e);
+										}finally{
+											if(handler != null) {
+												handler.release();
+												handler = null;
+											}
+										}
+									}
+								} else {
+									queryObjects( soluctions, sub, 1, damsClient, doc );
+								}
+							}
+							
+						}catch (Exception e) {
+							throw new Exception ("Failed to retrieve linked records for " + sub, e);
 						}
-					}catch (Exception e) {
-						log.error("Failed to retrieve record " + sub, e);
 					}
 				}
 			}
-		}
-		
-		if ( hasBn ) {
-			// has blankNode subject, query one level down.
-			queryObjects ( soluctions, oid, ++le, damsClient );
+			
+			if ( hasBn ) {
+				// for blankNode ComplexSubject (mads elements), query five level down, otherwise query one level down
+				Node node = parent.selectSingleNode("/rdf:RDF/*[string-length(@rdf:about) > 0]") ;
+				if (node != null && node.getPath().indexOf("/mads:") >= 0 && le <= 1) {
+					for (int l = 2; l <= 6; l++) 
+						queryObjects ( soluctions, oid, l, damsClient, parent);
+				} else
+					queryObjects ( soluctions, oid, ++le, damsClient, parent);
+					
+			}
 		}
 	}
-	
+
+	private String[] getQueries (String oid, int level, Document doc) {
+		String sparqlPrex = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+				"PREFIX mads: <http://www.loc.gov/mads/rdf/v1#> " +
+				"PREFIX dams: <http://library.ucsd.edu/ontology/dams#>";
+		String bnId = "";
+		int le = level;
+
+		if (level <= 1 ) {
+			String[] sparqls = {"SELECT ?sub WHERE { ?sub ?pre <" + oid + "> }"};
+			return sparqls;
+		} else if (level == 2 ){
+			// to retrieve ComplexSubjects linked records
+			String sparqlCS = sparqlPrex + " SELECT ?sub WHERE { ?sub mads:componentList ?bn . ?bn rdf:first <" + oid + "> }";
+			// to retrieve Relationship linked records: names and roles
+			String sparqlRS = sparqlPrex + " SELECT ?sub WHERE { ?sub dams:relationship ?bn . ?bn  ?pre <" + oid + "> } ";
+
+			Node node = doc.selectSingleNode("/rdf:RDF/*[contains(name(), 'Name') or contains(name(), 'Authority')]");
+			if ( node != null ) {
+				String[] sparqls = {sparqlCS, sparqlRS};
+				return sparqls;
+			} else {
+				String[] sparqls = {sparqlCS};
+				return sparqls;
+			}
+		} else {
+			String sparqlCS = sparqlPrex +  " SELECT ?sub WHERE { ";
+			for ( int i = 0; i < le; i++ ) {
+				// lookup linked records with blankNode: ComplexSubjects 
+				if ( i == 0 ) {
+					bnId = "_bn" + i;
+					sparqlCS += " ?sub mads:componentList ?bn . ";
+					sparqlCS += " ?bn rdf:rest ?" + bnId;
+				} else if ( i == le - 1 ) {
+					sparqlCS += " . ?" + bnId + " rdf:first <" + oid + "> ";
+				} else {
+					String tmpBnId = "_bn" + i;
+					sparqlCS += " . ?" + bnId + " rdf:rest ?" + tmpBnId;
+					bnId= tmpBnId;
+				}
+			}
+			sparqlCS += " }";
+			String[] sparqls = {sparqlCS};
+			return sparqls;
+		}
+	}
+
 	private boolean hasChange (String data, String oData) throws SAXException, IOException {
 		XMLUnit.setIgnoreWhitespace(true);
         XMLUnit.setIgnoreAttributeOrder(true);
