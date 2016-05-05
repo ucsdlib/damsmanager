7f7256492b517465eb7491b7632a99c6589d4f9e
lsitu <lsitu@ucsd.edu>
2016-05-05 16:12:10 -0700

    Added rules to exclude the alternate files from derivative creation. DM-170

--- src/edu/ucsd/library/xdre/collection/DerivativeHandler.java
+++ src/edu/ucsd/library/xdre/collection/DerivativeHandler.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javax.security.auth.login.LoginException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import edu.ucsd.library.xdre.utils.Constants;
@@ -107,16 +106,15 @@ public class DerivativeHandler extends CollectionHandler{
 	        	String use = dFile.getUse();
 				String fileID = dFile.getId();
 
+	        	DamsURI fileURI = DamsURI.toParts(fileID, dFile.getObject());
+
 				// Create derivative for the designated source file or master image files (source/alternative?), service video files (mp4) etc.
-	        	if((file!=null && file.length()>0 && fileID.endsWith(file)) || 
-	        			StringUtils.isBlank(file) && use != null && !use.startsWith("data") && (use.endsWith(Constants.SOURCE) || fileID.indexOf("/1.")>0 )){
+	        	if( isDerivativesRequired(fileURI.getFileName(), use) ){
 		        	totalFiles += 1;
 					
 	    	    	List<String> sizesList = new ArrayList<String>();
 	    	    	if(reqSizes != null)
 	    	    		sizesList.addAll(Arrays.asList(reqSizes));	        		
-	        		
-		        	DamsURI fileURI = DamsURI.toParts(fileID, dFile.getObject());        		
 
         			String oid = fileURI.getObject();
         			String cid = fileURI.getComponent();
