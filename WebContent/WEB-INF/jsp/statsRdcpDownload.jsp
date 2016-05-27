49af39b7b18a03c49aa4ae58fc18e0cb0fbc06de
lsitu <lsitu@ucsd.edu>
2016-05-27 07:45:34 -0700

    Update DAMS Statistics to support statistics for RDCP collections. DM-146

---
+++ WebContent/WEB-INF/jsp/statsRdcpDownload.jsp
@@ -0,0 +1,69 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"%>
+<%@ page errorPage="/jsp/errorPage.jsp"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<jsp:include flush="true" page="/jsp/libhtmlheader.jsp" />
+<body style="background-color:#fff;">
+<jsp:include flush="true" page="/jsp/libanner.jsp" />
+<table align="center" cellspacing="0px" cellpadding="0px" class="bodytable">
+<tr><td>
+<div id="tdr_crumbs">
+	<div id="tdr_crumbs_content">
+		<ul>
+			<li><a href="http://libraries.ucsd.edu">Library Home</a></li>
+			<li><a href="${model.clusterHost}/dc">Digital Library Collections</a></li>
+			<li><a href="/damsmanager/stats.do">DAMS Statistics</a></li>
+			<li>Item Usage</li>
+		</ul>
+	</div><!-- /tdr_crumbs_content -->
+	
+	<!-- This div is for temporarily writing breadcrumbs to for processing purposes -->
+		<div id="temporaryBreadcrumb" style="display: none">
+	</div>
+</div><!-- /tdr_crumbs -->
+<div id="main" class="gallery" align="center">
+	<div class="main-title">UCSD Library Digital Asset Management System Statistics</div>
+	<div id="container"></div>
+	<div class="tab-container">
+		<div class="tab-title">RDCP Objects Usage by Month</div>
+		<table cellspacing=0 cellpadding=3 border=0 width=100%>
+			<tbody id="stats-tab">
+				<tr class="tab-banner">
+					<th class="tab-banner-first">Collection</th>
+					<th class="tab-banner-first">Object title</th>
+					<th class="tab-banner-first">Component title</th>
+					<th class="tab-banner-first">ARK</th>
+					<c:forEach var="monItem" items="${model.periodsList}">
+						<th>${monItem}</th>
+					</c:forEach>
+				</tr>
+				<c:forEach var="sItem" items="${model.data}">
+					<tr style="background-color:#f5fcfc;">
+						<td style="text-align:left;paddings:0ps 2px;">${sItem.collectionTitle}</td>
+						<td style="text-align:left;paddings:0ps 2px;">${sItem.title}</td>
+						<td style="text-align:left;paddings:0ps 2px;">${sItem.componentTitle}</td>
+						<td style="text-align:left;paddings:0ps 2px;">${sItem.subjectId}</td>
+						<c:forEach var="v" items="${sItem.numOfViews}">
+							<td><fmt:formatNumber value="${v}" pattern="#,###" /></td>
+						</c:forEach>
+					</tr>
+				</c:forEach>
+			</tbody>
+		</table>
+		<div class="export">
+			<a href="/damsmanager/statsRdcpDownload.do?start=${model.start}&export">
+				<img src="images/excel-icon.png" border="0" width="16px" />
+				<span style="display:table-cell;vertical-align:top;font-size:11ps;font-weight:bold;">&nbsp;Export SCV</span></a>
+		</div>
+	</div>
+	<div id="message" align="left" class="errorBody">${model.message}</div>
+</div>
+</td>
+</tr>
+</table>
+<jsp:include flush="true" page="/jsp/libfooter.jsp" />
+</body>
+</html>
---
+++ src/edu/ucsd/library/xdre/web/StatsRdcpDownloadController.java
@@ -0,0 +1,239 @@
+package edu.ucsd.library.xdre.web;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import edu.ucsd.library.xdre.statistic.analyzer.Statistics;
+import edu.ucsd.library.xdre.statistic.beans.RdcpStatsDownload;
+import edu.ucsd.library.xdre.statistic.beans.RdcpStatsDownloadSummary;
+import edu.ucsd.library.xdre.statistic.report.StatsUsage;
+import edu.ucsd.library.xdre.utils.Constants;
+import edu.ucsd.library.xdre.utils.DAMSClient;
+
+
+ /**
+ * Class StatsRdcpDownloadController to summarize RDCP file download for rdcp statistics
+ *
+ * @author lsitu@ucsd.edu
+ */
+public class StatsRdcpDownloadController implements Controller {
+	public static final SimpleDateFormat DB_FORMAT = new SimpleDateFormat(Statistics.DATE_FORMAT);
+	
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, DocumentException {
+		
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		String startDate = request.getParameter("start");
+		String export = request.getParameter("export");
+		String message = "";
+		Connection con = null;
+		Calendar sCal = Calendar.getInstance();
+		Calendar eCal = Calendar.getInstance();
+		Date sDate = null;
+		StringBuilder strBuf = null;
+		SimpleDateFormat dbFormat = null;
+
+		DAMSClient damsClient = null;
+		try {
+			
+			dbFormat = new SimpleDateFormat(Statistics.DATE_FORMAT);
+			if(startDate != null && (startDate=startDate.trim()).length() > 0){
+				sDate = dbFormat.parse(startDate);
+				sCal.setTime(sDate);
+			}else{
+				//Default for one year back.
+				sCal.add(Calendar.YEAR, -1);
+				sCal.add(Calendar.MONTH, 1);
+				sCal.set(Calendar.DATE, 1);
+			}
+
+			con = Constants.DAMS_DATA_SOURCE.getConnection();
+
+			damsClient = new DAMSClient(Constants.DAMS_STORAGE_URL);
+			List<RdcpStatsDownloadSummary> statsDownloadSums = getRdcpStats(con, sCal.getTime(), eCal.getTime(), damsClient);
+	
+			if (export == null) {
+				if (statsDownloadSums.size() > 0) {
+					model.put("periodsList", statsDownloadSums.get(0).getPeriods());
+					model.put("data", statsDownloadSums);
+				} else {
+					model.put("periodsList", new ArrayList<String>());
+					model.put("data", new ArrayList<RdcpStatsDownloadSummary>());
+				}
+			} else {
+				int count = 0;
+				strBuf = new StringBuilder();
+				for(RdcpStatsDownloadSummary statsItemSum : statsDownloadSums) {
+					if (count == 0) {
+						strBuf.append("UCSD Library RDCP File Download Statistics By Month\n");
+						strBuf.append("Collection,Object title,Component title,ARK");
+						for (String period : statsItemSum.getPeriods()) {
+							strBuf.append("," + Statistics.escapeCsv(period));
+						}
+						strBuf.append("\n");
+					}
+					List<Integer> stats = statsItemSum.getNumOfViews();
+					strBuf.append(
+							Statistics.escapeCsv(statsItemSum.getCollectionTitle()) + "," + 
+							Statistics.escapeCsv(statsItemSum.getTitle()) + "," +
+							Statistics.escapeCsv(statsItemSum.getComponentTitle()) + "," +
+							Statistics.escapeCsv(statsItemSum.getSubjectId())
+							); 
+					for (Integer stat : stats) {
+						strBuf.append("," + stat );
+					}
+					strBuf.append( "\n" );
+					count++;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			message += "InternalError: " + e.getMessage();
+		} catch (Exception e) {
+			e.printStackTrace();
+			message += "Error: " + e.getMessage();
+		}finally{
+			if (damsClient != null) {
+				damsClient.close();
+			}
+			Statistics.close(con);
+			con = null;
+		}
+
+		if(export == null){
+			model.put("message", message);
+			model.put("start", dbFormat.format(sCal.getTime()));
+			return new ModelAndView("statsRdcpDownload", "model", model);
+		}else{
+			if(message != null && message.length() > 0)
+				strBuf.append(message);
+
+			OutputStream out = response.getOutputStream();
+			response.setHeader("Content-Disposition", "inline; filename=rdcp_stats.csv");
+			response.setContentType("text/csv");
+			out.write(strBuf.toString().getBytes());
+			out.close();
+			return null;
+		}
+	}
+
+	private static List<RdcpStatsDownloadSummary> getRdcpStats(Connection con, Date sDate, Date eDate, DAMSClient damsClient) throws SQLException {
+		//RDCP unique items usage
+		List<RdcpStatsDownloadSummary> statsDownloadSums = new ArrayList<>();
+
+		String period = null;
+		String sid = null;
+		String cid = null;
+		String fid = null;
+		int numOfViews = 0;
+		List<RdcpStatsDownload> statsItems = null;
+		List<String> periods = new ArrayList<>();
+		List<String> filePaths = new ArrayList<>();
+		Map<String, List<RdcpStatsDownload>> statsItemsMap = new HashMap<>();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try{
+			ps = con.prepareStatement(StatsUsage.RDCP_FILE_DOWNLOAD_POPULARITY_QUERY.replace("PERIOD_PARAM", StatsUsage.MONTHLY_FORMAT));
+			
+			ps.setString(1, DB_FORMAT.format(sDate));
+			ps.setString(2, DB_FORMAT.format(eDate));
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				period = rs.getString("period");
+				sid = rs.getString("object_id");
+				cid = rs.getString("comp_id");
+				numOfViews = rs.getInt("num_view");
+
+				String filePath = sid + "/" + (StringUtils.isBlank(cid) || cid.equals("0") ? "" : cid) + "/" + fid;
+				if(!filePaths.contains(filePath))
+					filePaths.add(filePath);
+				if(!periods.contains(period))
+					periods.add(period);
+
+				statsItems = statsItemsMap.get(filePath);
+				if(statsItems == null) {
+					statsItems = new ArrayList<>();
+					statsItemsMap.put(filePath, statsItems);
+				}
+				statsItems.add (new RdcpStatsDownload( period, sid, cid, fid, numOfViews ));
+			}
+		}finally{
+			if(rs != null){
+				try{
+					rs.close();
+					rs = null;
+				}catch(SQLException e){}
+			}
+			if(ps != null){
+				try{
+					ps.close();
+					ps = null;
+				}catch(SQLException e){}
+			}
+		}
+
+		// order the stats for each month
+		Collections.sort(periods);
+		for (String filePath : filePaths) {
+			Integer[] stats = new Integer[periods.size()];
+			for (int j=0; j < stats.length; j++)
+				stats[j] = new Integer(0);
+
+			statsItems = statsItemsMap.get(filePath);
+			for(RdcpStatsDownload statsItem : statsItems) {
+				int idx = periods.indexOf(statsItem.getPeriod());
+				if (idx >= 0)
+					stats[idx] = statsItem.getNumOfViews();
+			}
+
+			String colTitle = "";
+			String objTitle = "";
+			String compTitle = "";
+			String[] paths = filePath.split("\\/");
+			try {
+				sid = paths[0];
+				Document doc = Statistics.cacheGet(sid);
+				if (doc == null) {
+					doc = Statistics.getRecordForStats(sid);
+					Statistics.cacheAdd(sid, doc);
+				}
+				colTitle = Statistics.getTextValue (doc, "//doc/arr[@name='collection_name_tesim']/str");
+				objTitle = Statistics.getTitleFromJson(Statistics.getTextValue (doc, "//doc/arr[@name='title_json_tesim']/str"));
+				if (StringUtils.isNotBlank(paths[1])) {
+					String titleName = "component_" + paths[1] + "_title_json_tesim";
+					compTitle = Statistics.getTitleFromJson(Statistics.getTextValue (doc, "//doc/arr[@name='" + titleName + "']/str"));
+				}
+				RdcpStatsDownloadSummary statsItemSum = new RdcpStatsDownloadSummary (colTitle, objTitle, compTitle, 
+						paths[0], paths[1], paths[2], periods, Arrays.asList(stats));
+				statsDownloadSums.add(statsItemSum); 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return statsDownloadSums;
+	}
+}
